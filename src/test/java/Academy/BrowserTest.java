92408e4acc86cd598d7ead92fd87df260bbbc148
bgr-su <80604355+bgr-su@users.noreply.github.com>
2023-04-06 14:23:33 +0200

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -16,10 +16,13 @@ public class BrowserTest {
 	@Test
 	public void getData()
 	{
-		System.out.println("Hello Guys");
+		System.out.println("Hello Guys Test");
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		System.out.println("Test1");
 		WebDriver driver =new ChromeDriver();
+		System.out.println("Test2");
 		driver.get("https://testapp-dops.azurewebsites.net/webapp/");
+		System.out.println("Test3");
 		String text =driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
 		System.out.println("Test");
